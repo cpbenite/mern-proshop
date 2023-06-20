c32bd26c46fc580f0bd0c115e678ef2ec511192a
christian.benitez <christian.p.benitez@gmail.com>
2023-06-20 14:09:01 -0700

    Initialize header, footer, bootstrap.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
