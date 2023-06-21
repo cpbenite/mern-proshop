33d3ecd0a0f2fd1ca6461efa392137f206c7c902
christian.benitez <christian.p.benitez@gmail.com>
2023-06-20 18:49:33 -0700

    Initialize backend server.

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import products from './data/products.js'
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const { id } = req.params
+  const product = products.find(p => p._id === id)
+  res.json(product)
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
