e46711ed3d9ee39c26b169b2948520066e3edb99
christian.benitez <christian.p.benitez@gmail.com>
2023-06-20 19:07:14 -0700

    Initialize mongo db.

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
+    console.log(`Connected to MongoDB: ${conn.connection.host}.`)
+  } catch(err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+}
+
+export default connectDb
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+// import cors from 'cors'
 import products from './data/products.js'
+import connectDb from './config/db.js'
+
+connectDb();
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('hello')
@@ -22,7 +27,6 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find(p => p._id === id)
   res.json(product)
 })
-
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
