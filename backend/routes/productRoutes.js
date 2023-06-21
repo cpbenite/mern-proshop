43a57201cf1f7b337d3ad7cf867729d883c82eec
christian.benitez <christian.p.benitez@gmail.com>
2023-06-20 20:54:31 -0700

    Add custom error middleware.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,9 +14,10 @@ router.get('/:id', asyncHandler(async (req, res) => {
 
   if (product) {
     res.json(product)
+  } else {
+    res.status(404);
+    throw new Error('Resource not found')
   }
-  
-  res.status(404).json({ message: 'Product not found' })
 }))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config()
 import cors from 'cors'
 import productRoutes from './routes/productRoutes.js'
 import connectDb from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 connectDb();
 
@@ -19,6 +20,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
