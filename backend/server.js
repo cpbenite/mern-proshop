b1c4cd2e4d520614eb1bd39f835d278f4c77f347
christian.benitez <christian.p.benitez@gmail.com>
2023-06-21 17:39:49 -0700

    Setup redux toolkit. Fetch all products.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
-import cors from 'cors'
+// import cors from 'cors'
 import productRoutes from './routes/productRoutes.js'
 import connectDb from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+// app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('hello')
