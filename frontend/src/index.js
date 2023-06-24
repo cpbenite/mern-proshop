2fc94b2fc4fa42bf2111a07fe9772e0292552e11
christian.benitez <christian.p.benitez@gmail.com>
2023-06-24 16:06:29 -0700

    Protect routes for checkout steps. Add Payment Screen.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,8 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PrivateRoute from './components/PrivateRoute';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +29,12 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path='/shipping' element={<ShippingScreen />} />
+      
+
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
+      </Route>
     </Route>
   )
 )
