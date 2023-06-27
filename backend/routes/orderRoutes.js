995631587871c21d4ce87d3a10417541b85d4d82
christian.benitez <christian.p.benitez@gmail.com>
2023-06-27 00:44:52 -0700

    Initialize order controller and routes.

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getAllOrders
+} from '../controller/orderController.js'
+import { admin, protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getAllOrders)
+router.route('/myorders').get(protect, getMyOrders)
+router.route('/:id').get(protect, admin, getOrderById)
+router.route('/:id/pay').put(protect, admin, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
+
+export default router
