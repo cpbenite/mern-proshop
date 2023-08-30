bfc795eff1d89eba62f13f1660e65014dc048d2f
christian.benitez <christian.p.benitez@gmail.com>
2023-08-30 11:37:44 -0700

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('hello')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -44,6 +40,14 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 const __dirname = path.resolve()
 console.log(__dirname)
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
